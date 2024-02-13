ffcec99b3b4a1ee1d97c10cd18e346cc537ba7f8
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-02-13 11:58:41 -0600

    adding pencil to show color

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -5,6 +5,10 @@ export const ColorPicker = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='Page' title='Color Picker' />
+      <div className='text-center'>
+        <div id="preview" />
+        
+      </div>
     </div>
   );
 };
