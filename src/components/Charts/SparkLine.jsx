8db5b6838df800d21550944522a665e651022d44
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-02-12 18:18:40 -0600

    stacked chart using material ui

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -2,36 +2,6 @@ import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-re
 import { SparkLineChart } from '@mui/x-charts/SparkLineChart';
 import React from 'react';
 
-// export class SparkLine extends React.Component {
-//   render(){
-//     const { id, height, width, color, data, type, currentColor } = this.props
-//     return(
-//       <SparklineComponent
-//       id={id}
-//       height={height}
-//       width={width}
-//       lineWidth={1}
-//       valueType='Numeric'
-//       fill={color}
-//       border={{ color: currentColor, width: 2 }}
-//       dataSource={data}
-//       xName='x'
-//       yName='y'
-//       type={type}
-//       tooltipSettings={{
-//         visible: true,
-//         format: '${x} : data ${y}',
-//         trackLineSettings: {
-//           visible: true
-//         }
-//       }}
-//     >
-//       <Inject services={[ SparklineTooltip ]} />
-//     </SparklineComponent>
-//     );
-//   }
-// }
-
 export const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
   return (
     <>
--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
 import { stackedCustomSeries, stackedPrimaryYAxis, stackedPrimaryXAxis } from '../../data/dummy';
+import { BarChart } from '@mui/x-charts';
 
 export const Stacked = ({ width, height }) => {
   return (
-    <ChartComponent
-      height={height}
-      width={width}
-    >
-      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
-    </ChartComponent>
+    <>
+      {/* <ChartComponent
+        height={height}
+        width={width}
+      >
+        <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      </ChartComponent> */}
+      <BarChart
+        series={[
+          { data: [1, 6, 5, 8, 1, 7, 4], stack: 'A', color: '#4ade80'},
+          { data: [3, 1, 8, 7, 2, 1, 9], stack: 'A', color: '#e5e7eb' },
+        ]}
+        width={400}
+        height={350}
+      />
+    </>
   );
 };
