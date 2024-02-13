41eec410cd63746eed8f6253f15adc040fb116a4
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-02-12 19:41:33 -0600

    implementing search in the employees table

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,4 +1,4 @@
-import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids';
 import { employeesData, employeesGrid } from '../data/dummy';
 import { Header } from '../components';
 
@@ -7,17 +7,18 @@ export const Employees = () => {
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='Page' title='Employees' />
       <GridComponent
-        id='gridcomp'
         dataSource={employeesData}
         allowPaging
         allowSorting
+        toolbar={['Search']}
+        width='auto'
       >
         <ColumnsDirective>
           { employeesGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           )) }
         </ColumnsDirective>
-        <Inject services={[ Page, Search]} />
+        <Inject services={[ Page, Search, Toolbar ]} />
       </GridComponent>
     </div>
   );
