639ca0beddbe602eb3d7d2d9a06769b5385942a0
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-02-13 11:48:16 -0600

    text editor component

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -6,6 +6,9 @@ export const Editor = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='Page' title='Editor' />
+      <RichTextEditorComponent>
+        <Inject services={[ HtmlEditor, Toolbar, Image, Link, QuickToolbar ]} />
+      </RichTextEditorComponent>
     </div>
   );
 };
